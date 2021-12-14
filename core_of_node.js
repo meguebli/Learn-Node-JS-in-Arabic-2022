7cca799b74249cbe5d8a25fbb6bb8a02472f019e
Mohamed Nassar <86965432+mnassarhub@users.noreply.github.com>
2021-12-14 14:44:51 +0200

    Add files via upload

---
+++ core_of_node.js
@@ -0,0 +1,15 @@
+function sayHello() {
+	console.log(`Hello`);
+}
+
+// sayHello(); //global
+
+// console.log(1 + 9); //global
+
+// console.log(window);
+
+// console.log(global);
+
+console.log(process); // node
+
+console.log(document); // browser
